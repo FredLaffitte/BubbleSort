58c9e93cf5824a7bd59d53e3ebf81355dfd8a0e2
Frederico Laffitte <frederico.laffitte@supergasbrascombr.onmicrosoft.com>
2020-10-17 17:52:43 -0300

    Adicionar arquivos de projeto.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=====================================================");
+            Console.WriteLine("===============utilizando Bubble Sort================");
+            Console.WriteLine("=====================================================");
+            Console.WriteLine("=====Ordenando a sequência: 69, 55, 45, 98, 13=======");
+            int[] arr = { 69, 55, 45, 98, 13 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.Write("Ordenação -> ");
+            foreach (int p in arr)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+    }
+}
